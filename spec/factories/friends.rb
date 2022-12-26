908499eb4a3abfdd81343ca9b3dc82ac88511b7f
johnnydangfit <johnny.dang.goldenowl@gmail.com>
2022-12-26 23:54:31 +0700

    Fatory bot

--- spec/factories/friends.rb
+++ spec/factories/friends.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
+  # factory :friend do
+  #   first_name {"john"}
+  #   last_name {"dang"}
+  #   email {"johnny@example.com"}
+  # end
+
   factory :friend do
-    first_name { Faker::Name.first_name }
+    first_name {Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    sequence(:email) { Faker::Internet.email }
+    sequence(:email) { Faker::Internet.safe_email }
   end
 end
--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 require 'friend'
 RSpec.describe Friend, type: :model do
+  # shoulda-matchers
   describe "validation" do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to validate_presence_of(:last_name) }
@@ -9,5 +10,19 @@ RSpec.describe Friend, type: :model do
     it { is_expected.to validate_presence_of(:phone) }
     it { is_expected.to validate_length_of(:phone).is_at_least(10) }
     it { is_expected.to validate_length_of(:phone).is_at_most(15) }
+  end
+
+  #fatory bot
+  context "validation tests" do
+    let(:friend) {build(:friend)}
+    byebug
+    it 'ensure last name presence' do
+      expect(friend.save).to eq(false)
+    end
+
+    it 'ensure first name presence' do
+      friend.last_name = nil
+      expect(friend.save).to eq(false)
+    end
   end
 end
